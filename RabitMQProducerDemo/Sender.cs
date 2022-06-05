91e7e9c8204561667aabf1563cc59d2c33b64219
Prabhat Maurya <prabhat.maurya@in.abb.com>
2022-06-05 20:17:40 +0530

    Renamed solution and project name

--- RabitMQProducerDemo/Sender.cs
+++ RabitMQProducerDemo/Sender.cs
@@ -4,25 +4,25 @@ using System.Text;
 
 namespace RabitMQProducerDemo
 {
-    class Program
+    public class Sender
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory()
             {
                 HostName = "localhost"
             };
-            using(var connection =factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "HelloTestQ1", durable: false, exclusive: false, autoDelete: false, arguments:null);
+                channel.QueueDeclare(queue: "HelloTestQ1", durable: false, exclusive: false, autoDelete: false, arguments: null);
                 string message = "Hello World";
                 var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(exchange: "", 
+                channel.BasicPublish(exchange: "",
                                      routingKey: "HelloTestQ1",
                                      basicProperties: null,
-                                     body:body);
+                                     body: body);
                 Console.WriteLine("[x] sent  {0}", message);
             }
             Console.WriteLine(" Press [enter] to exit.");
