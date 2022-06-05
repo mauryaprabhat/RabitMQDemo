262e4493ad17f3aa7bbdc72c89b71b3773d3bd5f
Prabhat Maurya <prabhat.maurya@in.abb.com>
2022-06-05 11:32:31 +0530

    Added the sender

--- RabitMQProducerDemo/Program.cs
+++ RabitMQProducerDemo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabitMQProducerDemo
 {
@@ -6,7 +8,25 @@ namespace RabitMQProducerDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using(var connection =factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "HelloTestQ1", durable: false, exclusive: false, autoDelete: false, arguments:null);
+                string message = "Hello World";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", 
+                                     routingKey: "HelloTestQ1",
+                                     basicProperties: null,
+                                     body:body);
+                Console.WriteLine("[x] sent  {0}", message);
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
